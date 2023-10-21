711bb79c6e1251a9099b53b0b86699edbd036459
Saiful Islam <96956228+saifulislam735@users.noreply.github.com>
2023-10-21 15:07:56 +0600

    Update meal.js

--- js/meal.js
+++ js/meal.js
@@ -1,15 +1,4 @@
-const searchMeal = () => {
-    const getSearch = document.getElementById('search-field').value;
-    // const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${getSearch}`;
-    // console.log(url)
-    // fetch(url)
-    //     .then(res => res.json())
-    //     .then(data=>loadData(data))
-    const getDiv = document.getElementById('mealsContainer');
-    getDiv.innerText = ' ';
-    loadData(getSearch)
 
-}
 const loadData = (foodName) => {
     // console.log(foodName)
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`;
@@ -29,7 +18,12 @@ const getTheData = (foods) => {
                 <div class="card-body">
                     <h5 class="card-title">${food.strMeal}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit longer.</p>
+                        lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <!-- Button trigger modal -->
+                    <button onclick="modalDataGate(${food.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal"     data-bs-target="#staticBackdrop">
+                        Details
+                    </button>
                 </div>
             </div>
         </div>
@@ -37,5 +31,27 @@ const getTheData = (foods) => {
         getDiv.appendChild(addDiv);
     }
 
+}
+const modalDataGate = (mealId) => {
+    const mealUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(mealUrl)
+        .then(res => res.json())
+        .then(data => modalDataUpdate(data.meals[0]));
+}
+
+const modalDataUpdate = (modalData) => {
+    const modalTitle = document.getElementById('modal-title');
+    modalTitle.innerText = modalData.strMeal;
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = `
+    <img src="${modalData.strMealThumb}" class="img-fluid" alt="">
+    `;
+    console.log(modalData);
+}
+const searchMeal = () => {
+    const getSearch = document.getElementById('search-field').value;
+    const getDiv = document.getElementById('mealsContainer');
+    getDiv.innerText = ' ';
+    loadData(getSearch)
 }
 loadData('fish');
